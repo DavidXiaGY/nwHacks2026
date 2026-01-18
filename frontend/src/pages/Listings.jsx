f9df78ad6b6d87ed3a52f96aa76fd40f67284525
enq <enq@sfu.ca>
2026-01-18 02:53:40 -0800

    adjusted image in header of orphanages page

--- frontend/src/pages/Listings.jsx
+++ frontend/src/pages/Listings.jsx
@@ -3,11 +3,16 @@ import OrphanageCard from '../components/OrphanageCard'
 function Listings(){
     return(
         <div className="flex flex-col">
-            <div className="header p-[48px] pb-[48px] h-[400px] w-full flex flex-col justify-end bg-[url('/src/assets/orphanTrees.svg')] bg-[right_bottom] bg-no-repeat bg-[length:65%]">
-                <h1 className="text-[40px] font-bold font-redhatdisplay">
-                    Orphanages
-                </h1>
-                <p className="font-manrope text-[#12707C]">Browse verified organizations and support children in your community</p>
+            <div className="header h-[400px] w-full flex flex-col justify-end">
+                <div className="background-img absolute right-[-450px] z-[-1]">
+                    <img src="/src/assets/newTrees.svg" alt="Background"/>
+                </div>
+                <div className='p-[48px] pb-[48px]'>
+                    <h1 className="text-[40px] font-bold font-redhatdisplay">
+                        Orphanages
+                    </h1>
+                    <p className="font-manrope text-[#12707C]">Browse verified organizations and support children in your community</p>
+                </div>
             </div>
             <div className="p-[48px] border-t-2 border-[#06404D]">
                 <OrphanageCard orphanage={{ 
