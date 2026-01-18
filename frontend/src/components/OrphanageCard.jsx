06df123ad1cfaa13c1168b9a47aa1edc242c55e9
enq <enq@sfu.ca>
2026-01-17 21:30:57 -0800

    styled card

--- frontend/src/components/OrphanageCard.jsx
+++ frontend/src/components/OrphanageCard.jsx
@@ -1,13 +1,19 @@
 export default function OrphanageCard({ orphanage }) {
   return (
-    <div className="orphanage-card border border-[#06404D] p-4 bg-[#FFFCFA]">
-        <div className="border-2 border-[#06404D] p-4">
-            <h1 className="font-bold text-[3xl]">{orphanage.name}</h1>
-            <p>{orphanage.description}</p>
-            <button>View Angels</button>
-            <p>{orphanage.location}</p>
-            <p>Angels: {orphanage.angelCount}</p>
+    <div className="orphanage-card border border-[#06404D] p-4 bg-[#FFFCFA] max-w-[530px]">
+      <div className="inner-boarder border-2 border-[#06404D] p-4 font-manrope flex flex-col-reverse justify-between min-h-[270px] text-[16px]">
+        <div className="bottom">
+            <h1 className="font-bold text-[32px] mb-2 font-redhatdisplay">{orphanage.name}</h1>
+            <div className="sub-text flex flex-row justify-between">
+                <p className="max-w-[300px]">{orphanage.description}</p>
+                <button className="bg-[#F2ABA7] text-[#FFFCFA] px-4 py-2 rounded-lg">VIEW ANGELS</button>
+            </div>
         </div>
+        <div className="top text-[#12707C] flex flex-row justify-between mb-4">
+            <p>{orphanage.location}</p>
+            <p className="border border-[#06404D] px-4 py-2 rounded-lg">{orphanage.angelCount} Angels</p>
+        </div>
+      </div>
     </div>
   );
 }
