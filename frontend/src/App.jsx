9842348b62567e7a29413917bc8d92a00edf01b9
DavidXiaGY <davidxia1203@gmail.com>
2026-01-18 01:57:35 -0800

    implement nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import OrganizerUpload from './components/OrganizerUpload'
 import Listings from './pages/Listings'
 import OrphanageDetails from './pages/OrphanageDetails'
 import InteractiveMap from './pages/InteractiveMap'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/login" element={<LoginSignup />} />
         <Route path="/organizer-upload" element={<OrganizerUpload />} />
