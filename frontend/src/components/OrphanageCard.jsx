00b74d26de95d3073853e1268eb822718b0412aa
enq <enq@sfu.ca>
2026-01-18 02:23:25 -0800

    added header image and orphanage card styling

--- frontend/src/components/OrphanageCard.jsx
+++ frontend/src/components/OrphanageCard.jsx
@@ -1,17 +1,20 @@
 export default function OrphanageCard({ orphanage }) {
   return (
     <div className="orphanage-card border border-[#06404D] p-4 bg-[#FFFCFA] max-w-[530px]">
-      <div className="inner-boarder border-2 border-[#06404D] p-4 font-manrope flex flex-col-reverse justify-between min-h-[270px] text-[16px]">
+      <div className="inner-boarder border-2 border-[#06404D] p-[28px] font-manrope flex flex-col-reverse justify-between min-h-[270px] text-[16px]">
         <div className="bottom">
-            <h1 className="font-bold text-[32px] mb-2 font-redhatdisplay">{orphanage.name}</h1>
+            <h1 className="font-bold text-[32px] mb-2 mt-3 font-redhatdisplay">{orphanage.name}</h1>
             <div className="sub-text flex flex-row justify-between">
                 <p className="max-w-[300px]">{orphanage.description}</p>
-                <button className="bg-[#F2ABA7] text-[#FFFCFA] px-4 py-2 rounded-lg">VIEW ANGELS</button>
+                <button className="font-semibold bg-[#F2ABA7] text-[#FFFCFA] px-4 py-2 rounded-lg">VIEW ANGELS</button>
             </div>
         </div>
         <div className="top text-[#12707C] flex flex-row justify-between mb-4">
             <p>{orphanage.location}</p>
-            <p className="border border-[#06404D] px-4 py-2 rounded-lg">{orphanage.angelCount} Angels</p>
+            <div className="border border-[#F2ABA7] p-1">
+              <img src={orphanage.image} alt={orphanage.name} className="w-[75px] h-[81px] object-cover border-2 border-[#F2ABA7]"/>
+            </div>
+            {/* <p className="border border-[#06404D] px-4 py-2 rounded-lg">{orphanage.angelCount} Angels</p> */}
         </div>
       </div>
     </div>
--- frontend/src/pages/Listings.jsx
+++ frontend/src/pages/Listings.jsx
@@ -2,14 +2,23 @@ import OrphanageCard from '../components/OrphanageCard'
 
 function Listings(){
     return(
-        <div>
-            <OrphanageCard orphanage={{ 
-            name: "The Childen's Foundation",
-            location: "Vancouver, BC",
-            description: "Empowering youth through education and community.",
-            angelCount: 21,
-            }} />
-      </div>
+        <div className="flex flex-col">
+            <div className="header p-[48px] pb-[48px] h-[400px] w-full flex flex-col justify-end bg-[url('/src/assets/orphanTrees.svg')] bg-[right_bottom] bg-no-repeat bg-[length:65%]">
+                <h1 className="text-[40px] font-bold font-redhatdisplay">
+                    Orphanages
+                </h1>
+                <p className="font-manrope text-[#12707C]">Browse verified organizations and support children in your community</p>
+            </div>
+            <div className="p-[48px] border-t-2 border-[#06404D]">
+                <OrphanageCard orphanage={{ 
+                    name: "The Children's Foundation",
+                    location: "Vancouver, BC",
+                    description: "Empowering youth through education and community.",
+                    angelCount: 21,
+                    image: "src/assets/holly.svg"
+                }} />
+            </div>
+        </div>
     )
 }
 
